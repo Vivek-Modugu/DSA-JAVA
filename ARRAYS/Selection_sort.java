f18c44dcb4e3340f506d5692938a6e8d71bbc714
Vivek-Modugu <vivekreddy302006@gmail.com>
2026-09-08 07:05:30 +0530

    selection sort

---
+++ ARRAYS/Selection_sort.java
@@ -0,0 +1,23 @@
+
+
+public class Selection_sort {
+    public static void main(String[] args) {
+
+        int arr[] = {13,46,24,52,20,9};
+
+        for(int i=0;i<arr.length-1;i++){
+            int min = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j] < arr[min])
+                    min = j;
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
